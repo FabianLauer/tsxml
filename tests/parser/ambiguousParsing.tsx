db62cda6bfd474035a35d6431369dcb57a334879
Fabian Lauer <kontakt@softwareschmiede-saar.de>
2016-05-04 20:31:33 +0200

    add further test cases for open angle brackets in text nodes

--- tests/parser/ambiguousParsing.tsx
+++ tests/parser/ambiguousParsing.tsx
@@ -5,6 +5,21 @@ import * as xml from '../../src/index';
 import '../../src/test/jsx';
 
 
+class OpenAngleBracketAsOnlyTextNodeContent extends test.UnitTest {
+	protected async performTest() {
+		const textContent = '<',
+			  ast = await xml.Parser.parseStringToAst(`<a>${textContent}</a>`);
+		const wrapperNode = ast.getChildAtIndex(0) as xml.ast.ContainerNode<xml.ast.TextNode>;
+		await this.assert(wrapperNode instanceof xml.ast.Node, 'correct outer ast node type');
+		await this.assert(wrapperNode.tagName === 'a', 'outer ast node has correct tag name');
+		const textNode = wrapperNode.getChildAtIndex(0);
+		await this.assert(textNode instanceof xml.ast.Node, 'text node exists');
+		await this.assert(textNode instanceof xml.ast.TextNode, 'text node has correct ast node type');
+		await this.assert(textNode.content === textContent, `text node has correct content, got (type ${typeof textNode.content}) '${textNode.content}'`);
+	}
+}
+
+
 class OpenAngleBracketInTextNodeContent extends test.UnitTest {
 	protected async performTest() {
 		const textContent = '123<456',
@@ -12,6 +27,24 @@ class OpenAngleBracketInTextNodeContent extends test.UnitTest {
 		const wrapperNode = ast.getChildAtIndex(0) as xml.ast.ContainerNode<xml.ast.TextNode>;
 		await this.assert(wrapperNode instanceof xml.ast.Node, 'correct outer ast node type');
 		await this.assert(wrapperNode.tagName === 'a', 'outer ast node has correct tag name');
+		const textNode = wrapperNode.getChildAtIndex(0);
+		await this.assert(textNode instanceof xml.ast.Node, 'text node exists');
+		await this.assert(textNode instanceof xml.ast.TextNode, 'text node has correct ast node type');
+		await this.assert(textNode.content === textContent, `text node has correct content, got (type ${typeof textNode.content}) '${textNode.content}'`);
+	}
+}
+
+
+class OpenAngleBracketInTextNodeContentInNestedNode extends test.UnitTest {
+	protected async performTest() {
+		const textContent = '123<456',
+			  ast = await xml.Parser.parseStringToAst(`<a><b>${textContent}</b></a>`);
+		const outerNode = ast.getChildAtIndex(0) as xml.ast.ContainerNode<xml.ast.ContainerNode<xml.ast.TextNode>>;
+		await this.assert(outerNode instanceof xml.ast.Node, 'correct outer ast node type');
+		await this.assert(outerNode.tagName === 'a', 'outer ast node has correct tag name');
+		const wrapperNode = outerNode.getChildAtIndex(0);
+		await this.assert(wrapperNode instanceof xml.ast.Node, 'correct wrapper ast node type');
+		await this.assert(wrapperNode.tagName === 'b', 'wrapper ast node has correct tag name');
 		const textNode = wrapperNode.getChildAtIndex(0);
 		await this.assert(textNode instanceof xml.ast.Node, 'text node exists');
 		await this.assert(textNode instanceof xml.ast.TextNode, 'text node has correct ast node type');
@@ -55,7 +88,9 @@ export class TestRunner extends test.TestRunner {
 	constructor() {
 		super();
 		this.add(
+			new OpenAngleBracketAsOnlyTextNodeContent(),
 			new OpenAngleBracketInTextNodeContent(),
+			new OpenAngleBracketInTextNodeContentInNestedNode(),
 			new OpenAngleBracketInCommentNodeContent(),
 			new OpenAngleBracketInCDataSectionNodeContent()
 		);
