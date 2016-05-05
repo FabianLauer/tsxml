da921827db40efacc113541e6cc78b81fa985119
Fabian Lauer <kontakt@softwareschmiede-saar.de>
2016-05-05 20:46:05 +0200

    add comment node serialisation

--- src/ast/CommentNode.ts
+++ src/ast/CommentNode.ts
@@ -1,3 +1,12 @@
 import {TextNode} from './TextNode';
+import {IStringificationParams} from './IStringificationParams';
 
-export class CommentNode extends TextNode { }
+export class CommentNode extends TextNode {
+	/**
+	 * @override
+	 */
+	protected stringify(params: IStringificationParams, nodeIndentDepth?: number): string {
+		nodeIndentDepth = Math.max(nodeIndentDepth || 0, 0);
+		return `${CommentNode.generateIndentString(params.indentChar, nodeIndentDepth)}<!--${this.content}-->${params.newlineChar}`;
+	}
+}
