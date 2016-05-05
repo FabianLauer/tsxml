d1eac536ccb4fc105dd6af94bffdf7f9fb789aae
Fabian Lauer <kontakt@softwareschmiede-saar.de>
2016-05-05 16:22:11 +0200

    tests: add 'serialisation' test runner

--- tests/main.ts
+++ tests/main.ts
@@ -1,12 +1,16 @@
 import * as test from '../src/test';
 import * as parser from './parser/TestRunner';
 import * as real from './real/TestRunner';
+import * as serialisation from './serialisation/TestRunner';
 
 (async () => {
 	const parserTestRunner = new parser.TestRunner(),
-		  realTestRunner = new real.TestRunner();
+		  realTestRunner = new real.TestRunner(),
+		  serialisationTestRunner = new serialisation.TestRunner();
 	new test.CliRenderer(parserTestRunner);
 	await parserTestRunner.run();
+	new test.CliRenderer(serialisationTestRunner);
+	await serialisationTestRunner.run();
 	new test.CliRenderer(realTestRunner);
 	await realTestRunner.run();
 })();
---
+++ tests/serialisation/TestRunner.ts
@@ -0,0 +1,82 @@
+/// <reference path="../../typings/node/node" />
+import * as fs from 'fs';
+import * as test from '../../src/test';
+import * as xml from '../../src/index';
+
+
+abstract class FileTest extends test.UnitTest {
+	protected abstract getFullPathToFile(): string;
+	
+	protected async performTest() {
+		const serializedXml = FileTest.makeComparableString((await xml.Parser.parseStringToAst(await this.getSourceAsString())).toString());
+		await this.assert(serializedXml === FileTest.makeComparableString(await this.getExpectationAsString()), 'serialized XML meets expectation');
+		this.cleanup();
+	}
+	
+	
+	private static makeComparableString(str: string): string {
+		return str.trim().replace(/^(\r?\n)+/, '').replace(/(\r?\n)+$/, '').trim();
+	}
+	
+	
+	private async getFileContentAsString(): Promise<string>  {
+		if (typeof this.fileContent === 'string') {
+			return this.fileContent;
+		}
+		return new Promise<string>((resolve: (contentAsString: string) => void, reject: (reason: any) => void) => {
+			fs.readFile(this.getFullPathToFile(), (err: any, content: Buffer) => {
+				if (err) {
+					reject(err);
+				} else {
+					this.fileContent = content + '';
+					resolve(this.fileContent);
+				}
+			});
+		});
+	}
+	
+	
+	private async getSourceAsString(): Promise<string> {
+		return (await this.getFileContentAsString()).split(/\n@@/)[1];
+	}
+	
+	
+	private async getExpectationAsString(): Promise<string> {
+		return (await this.getFileContentAsString()).split(/\n@@/)[1];
+	}
+	
+	
+	private cleanup(): void {
+		this.fileContent = undefined;
+	}
+	
+	
+	private static splitRegex = /\n@@ *EXPECTATION *@@/i;
+	
+	
+	private fileContent: string;
+}
+
+
+@TestRunner.testName('Serialisation w/o Formatting')
+export class TestRunner extends test.TestRunner {
+	constructor() {
+		super();
+		this.prepareFileTestsForDirectory('xml/', FileTest);
+	}
+	
+	
+	private prepareFileTestsForDirectory(directoryPath: string, testBaseClass: typeof FileTest): void {
+		const basePath = './tests/serialisation/';
+		fs.readdirSync(basePath + directoryPath).forEach(path => {
+			const fullPathToFile = basePath + directoryPath + path;
+			if (!fs.statSync(fullPathToFile).isFile()) {
+				return;
+			}
+			this.add(new (class extends testBaseClass {
+				public get name() { return directoryPath + path; }
+				protected getFullPathToFile() { return fullPathToFile; }
+			}));
+		});
+	}
+}
