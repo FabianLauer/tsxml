3cde1a1b4d85d57a6fd69d5da7e2c991d4d0744c
Fabian Lauer <kontakt@softwareschmiede-saar.de>
2016-05-05 17:02:43 +0200

    add missing '@override'

--- src/ast/DeclarationOpenerNode.ts
+++ src/ast/DeclarationOpenerNode.ts
@@ -5,6 +5,9 @@ export class DeclarationOpenerNode extends Node {
 	public systemLiterals: string[] = [];
 	
 	
+	/**
+	 * @override
+	 */
 	protected stringify(params: IStringificationParams, nodeIndentDepth?: number): string {
 		nodeIndentDepth = Math.max(nodeIndentDepth || 0, 0);
 		return `${Node.generateIndentString(params.indentChar, nodeIndentDepth)}<!${this.tagName}${this.stringifyAttributes(nodeIndentDepth)}${this.stringifySystemLiterals()}>${params.newlineChar}`;
