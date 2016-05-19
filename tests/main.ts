94856d63544a2d5a5e86917117427fc08f814d7d
Fabian Lauer <kontakt@softwareschmiede-saar.de>
2016-05-19 11:09:22 +0200

    clean up main test runner file

--- tests/main.ts
+++ tests/main.ts
@@ -4,17 +4,29 @@ import * as parser from './parser/TestRunner';
 import * as real from './real/TestRunner';
 import * as serialisation from './serialisation/TestRunner';
 
+
+// all test runners that should be run:
+const testRunners: Array<typeof test.TestRunner> = [
+	ast.TestRunner,
+	parser.TestRunner,
+	real.TestRunner,
+	serialisation.TestRunner
+];
+
+
+/**
+ * Runs a single test runner.
+ */
+async function run(testRunner: test.TestRunner): Promise<void> {
+	new test.CliRenderer(testRunner);
+	await testRunner.run();
+}
+
+
+// init
 (async () => {
-	const astTestRunner = new ast.TestRunner(),
-		  parserTestRunner = new parser.TestRunner(),
-		  realTestRunner = new real.TestRunner(),
-		  serialisationTestRunner = new serialisation.TestRunner();
-	new test.CliRenderer(astTestRunner);
-	await astTestRunner.run();
-	new test.CliRenderer(parserTestRunner);
-	await parserTestRunner.run();
-	new test.CliRenderer(serialisationTestRunner);
-	await serialisationTestRunner.run();
-	new test.CliRenderer(realTestRunner);
-	await realTestRunner.run();
-})();
+	for (let i = 0; i < testRunners.length; i++) {
+		await run(new testRunners[i]);
+	}
+	process.exit();
+})();	
