ca0f4368994f302cdab16f09991887ffbfe95967
Fabian Lauer <kontakt@softwareschmiede-saar.de>
2016-05-28 10:46:19 +0200

    serialisation: don't emit a space before the end question mark in processing instructions

--- src/ast/ProcessingInstructionNode.ts
+++ src/ast/ProcessingInstructionNode.ts
@@ -7,6 +7,6 @@ export class ProcessingInstructionNode extends Node {
 	 */
 	protected stringify(params: IStringificationParams, nodeIndentDepth?: number): string {
 		nodeIndentDepth = Math.max(nodeIndentDepth || 0, 0);
-		return `${Node.generateIndentString(params.indentChar, nodeIndentDepth)}<?${this.tagName}${this.stringifyAttributes(nodeIndentDepth)} ?>${params.newlineChar}`;
+		return `${Node.generateIndentString(params.indentChar, nodeIndentDepth)}<?${this.tagName}${this.stringifyAttributes(nodeIndentDepth)}?>${params.newlineChar}`;
 	}
 }
