a1bb5ad10fff62bc639caea0e50e4b44d6b66fc4
Fabian Lauer <kontakt@softwareschmiede-saar.de>
2016-05-30 21:43:17 +0200

    xpath: add all ast files to the ast index file

--- src/xpath/ast.ts
+++ src/xpath/ast.ts
@@ -1,5 +1,29 @@
+import {Node} from './ast/Node';
+import {NodeSet} from './ast/NodeSet';
+import {ExecutableNode} from './ast/ExecutableNode';
 import {Expression} from './ast/Expression';
+import {Selector} from './ast/Selector';
+import {NodeSelector} from './ast/NodeSelector';
+import {RootNodeSelector} from './ast/RootNodeSelector';
+import {WildcardSelector} from './ast/WildcardSelector';
+import {WildcardSelectorType} from './ast/WildcardSelectorType';
+import {ComparisonOperator} from './ast/ComparisonOperator';
+import {PredicateOperation} from './ast/PredicateOperation';
+import {Predicate} from './ast/Predicate';
+import {ContextSelector} from './ast/ContextSelector';
 
 export {
-	Expression
+	Node,
+	NodeSet,
+	ExecutableNode,
+	Expression,
+	Selector,
+	NodeSelector,
+	RootNodeSelector,
+	WildcardSelector,
+	WildcardSelectorType,
+	ComparisonOperator,
+	PredicateOperation,
+	Predicate,
+	ContextSelector
 };
