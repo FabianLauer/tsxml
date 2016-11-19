08736b6e93013119d7544bb0adbd8f26b45cc9b0
Fabian Lauer <kontakt@softwareschmiede-saar.de>
2016-11-19 11:08:15 +0100

    cleanup & formatting in parser.ts

--- src/parser/Parser.ts
+++ src/parser/Parser.ts
@@ -16,10 +16,19 @@ import {ITagNameInfo} from './ITagNameInfo';
 /**
  * Parsers create a syntax tree from an XML string.
  * PARSER INTERNALS:
- * Parsers see every character in an XML string as a "token". This means that there is no tokenization stage, but rather just a quick (and lazy) mapping of characters to their line and column number. Even without tokenization, XML is fairly simple to parse due to its non-complex syntax. The absence of a tokenization stage means there are less dependencies, less coupling is necessary, which leads to lower maintenance time. Also, we're saving a few CPU cycles and some memory, although performance is not the primary factor for the decision against a dedicated tokenizer.
- * The public interface provided by the parser class encourages the use of static methods, such as `parseStringToAst(...)`, instead of manually creating and handling parser objects (at least for now). Also, these static methods enforce user code to await promises, even though parser code is not **yet** async, but it might become async when (and if) incremental parsing and streams are implemented (incremental parsing might use generators and allow async user code to interfere with the parser).
+ * Parsers see every character in an XML string as a "token". This means that there is no tokenization stage, but rather
+ * just a quick (and lazy) mapping of characters to their line and column number. Even without tokenization, XML is
+ * fairly simple to parse due to its non-complex syntax. The absence of a tokenization stage means there are less
+ * dependencies, less coupling is necessary, which leads to lower maintenance time. Also, we're saving a few CPU cycles
+ * and some memory, although performance is not the primary factor for the decision against a dedicated tokenizer.
+ * The public interface provided by the parser class encourages the use of static methods, such as
+ * `parseStringToAst(...)`, instead of manually creating and handling parser objects (at least for now). Also, these
+ * static methods enforce user code to await promises, even though parser code is not **yet** async, but it might become
+ * async when (and if) incremental parsing and streams are implemented (incremental parsing might use generators and
+ * allow async user code to interfere with the parser).
  * SYNTAX RULES:
- * Parsers can accept some syntax rules, however by default they expect XML (no void tags, unclosed tags are handled as if they were). To override default parsing rules, use the `addTagSyntaxRule(...)` (and similar) methods.
+ * Parsers can accept some syntax rules, however by default they expect XML (no void tags, unclosed tags are handled as
+ * if they were). To override default parsing rules, use the `addTagSyntaxRule(...)` (and similar) methods.
  */
 export class Parser {
 	/**
@@ -42,7 +51,8 @@ export class Parser {
 	 * Parses an XML string and returns the parser object that parsed the string.
 	 * @see Parser.parseStringToAst(...)
 	 * @param stringToParse The XML string to be parsed.
-	 * @param ruleSet The sytnax rule set to apply to the parser. Optional. The parser falls back to default XML parsing rules when no other rules are provided.
+	 * @param ruleSet The sytnax rule set to apply to the parser. Optional. The parser falls back to default XML parsing
+	 *                rules when no other rules are provided.
 	 */
 	public static async parseString(stringToParse: string, ruleSet?: SyntaxRuleSet | typeof SyntaxRuleSet): Promise<Parser> {
 		const parser = Parser.createForXmlString(stringToParse);
@@ -58,7 +68,8 @@ export class Parser {
 	 * Parses an XML string and returns a syntax tree.
 	 * @see Parser.parseString(...)
 	 * @param stringToParse The XML string to be parsed.
-	 * @param ruleSet The sytnax rule set to apply to the parser. Optional. The parser falls back to default XML parsing rules when no other rules are provided.
+	 * @param ruleSet The sytnax rule set to apply to the parser. Optional. The parser falls back to default XML parsing
+	 *                rules when no other rules are provided.
 	 */
 	public static async parseStringToAst(stringToParse: string, ruleSet?: SyntaxRuleSet | typeof SyntaxRuleSet): Promise<DocumentNode> {
 		return (await Parser.parseString(stringToParse, ruleSet)).getAst();
@@ -278,7 +289,8 @@ export class Parser {
 	
 	
 	/**
-	 * Finds the first occurence of a certain token after in the source XML string after a certain token index and returns the index of the searched token.
+	 * Finds the first occurence of a certain token after in the source XML string after a certain token index and
+	 * returns the index of the searched token.
 	 * @param token The token to find.
 	 * @param startIndex The index at which to start searching.
 	 */
@@ -304,7 +316,8 @@ export class Parser {
 	
 	
 	/**
-	 * Returns the container ast node the parser is currently parsing into, depending on the semantic context around the cursor. At the start and end of each parsing run, this will return the outermost `DocumentNode` of the syntax tree.
+	 * Returns the container ast node the parser is currently parsing into, depending on the semantic context around the
+	 * cursor. At the start and end of each parsing run, this will return the outermost `DocumentNode` of the syntax tree.
 	 */
 	protected getCurrentContainerNode(): ContainerNode<any> {
 		return this.currentContainerNode;
@@ -347,7 +360,8 @@ export class Parser {
 	
 	
 	/**
-	 * Raises an error. Use this method instead of throwing manually so errors can be logged or modified by the parser before it is thrown.
+	 * Raises an error. Use this method instead of throwing manually so errors can be logged or modified by the parser
+	 * before it is thrown.
 	 * @throws
 	 * @param error The error to raise.
 	 */
@@ -379,7 +393,8 @@ export class Parser {
 	
 	
 	/**
-	 * Returns all tag close modes allowed for a certain tag name. The returned modes are either defined by tag syntax rules or fall back to the default if no syntax rule for the given tag name exists.
+	 * Returns all tag close modes allowed for a certain tag name. The returned modes are either defined by tag syntax
+	 * rules or fall back to the default if no syntax rule for the given tag name exists.
 	 */
 	protected getAllowedTagCloseModesForTagName(tagName: string): TagCloseMode {
 		return this.getOverrideOrDefaultTagSyntaxRuleForTagName(tagName).getCloseMode();
@@ -492,18 +507,21 @@ export class Parser {
 	
 	
 	/**
-	 * Called when the parser is at an open angle bracket (`<`) and needs to decide how to parse upcoming tokens. This method looks ahead to decide
-	 * whether the open angle bracket is the beginning of an XML tag, or if it's the beginning of text node content, so either:
+	 * Called when the parser is at an open angle bracket (`<`) and needs to decide how to parse upcoming tokens.
+	 * This method looks ahead to decide whether the open angle bracket is the beginning of an XML tag, or if it's the
+	 * beginning of text node content, so either:
 	 *     <foo...
 	 *     ^       here
 	 * or:
 	 *     <foo><</foo>
 	 *          ^ here
 	 * 
-	 * Keep in mind that this method must *only* be called in these two cases, all other possible occurences of open angle brackets are handled in
-	 * more specific methods (namely when parsing CDATA or comments), which are not ambiguous (comments and CDATA nodes have delimiters that clearly
-	 * indicate where their content begins and ends, text nodes do not have this).
-	 * The same goes for attributes: An open angle bracket in a properly quoted attribute string is always going to be parsed as an attribute value.
+	 * Keep in mind that this method must *only* be called in these two cases, all other possible occurences of open
+	 * angle brackets are handled in more specific methods (namely when parsing CDATA or comments), which are not
+	 * ambiguous (comments and CDATA nodes have delimiters that clearly indicate where their content begins and ends,
+	 * text nodes do not have this).
+	 * The same goes for attributes: An open angle bracket in a properly quoted attribute string is always going to be
+	 * parsed as an attribute value.
 	 * An open angle bracket in an attribute value *that is not enclosed by quotes* is always a syntax error:
 	 *     <foo bar="1<2" />
 	 *                ^       OK, but does not concern this method
@@ -515,7 +533,8 @@ export class Parser {
 		//     the next token does not indicate a CDATA node, comment, PI or MDO
 		//   and:
 		//     there's another open angle bracket before the next occurence of a closing angle bracket
-		// assume that the current open angle bracket is text node content. In all other cases, assume that the current open angle bracket indicates
+		// assume that the current open angle bracket is text node content. In all other cases, assume that the current
+		// open angle bracket indicates
 		// the bginning of a new tag.
 		if (this.getNextToken() !== '!' && this.getNextToken() !== '?' &&
 			this.doesTokenOccurBeforeNextOccurenceOfOtherToken('<', '>', this.getCurrentTokenIndex() + 1))
@@ -528,8 +547,8 @@ export class Parser {
 	
 	
 	/**
-	 * Creates a new text node, appends it to the ast and parses all upcoming text into it. Stops parsing at the first character that can not be
-	 * considered text anymore. 
+	 * Creates a new text node, appends it to the ast and parses all upcoming text into it. Stops parsing at the first
+	 * character that can not be considered text anymore.
 	 */
 	protected parseIntoNewTextNode(): void {
 		const textNode = new ast.TextNode();
@@ -572,10 +591,12 @@ export class Parser {
 	 * Parses from the beginning of any kind of tag. The cursor is expected to point at the open angle bracket of the tag, such as:
 	 *     <xsl:stylesheet ...
 	 *     ^
-	 * Comments and CDATA sections are also supported by this method. Depending on the kind of tag (MDO, PI, normal, etc), this method will delegate parsing the tag to other more specific methods.
+	 * Comments and CDATA sections are also supported by this method. Depending on the kind of tag (MDO, PI, normal, etc),
+	 * this method will delegate parsing the tag to other more specific methods.
 	 */
 	protected parseFromBeginningOfTag(): void {
-		// Find out if we're dealing with a "normal" node here or with a MDO (markup declaration opener), PI (processing instruction) or comment.
+		// Find out if we're dealing with a "normal" node here or with a MDO (markup declaration opener), PI (processing
+		// instruction) or comment.
 		// We will not know whether the node is self closing, or if it has child nodes or text content, but
 		// we know just enough to delegate the node to a more dedicated parsing method depending on what the
 		// node actually is.
@@ -686,7 +707,8 @@ export class Parser {
 	protected parseFromBeginningOfCloseTag(): void { 
 		// Validate that we actually have a close tag:
 		if (this.getTokenRangeStartingAt(this.getCurrentTokenIndex(), 2) !== '</') {
-			this.raiseError(this.createUnexpectedTokenSyntaxErrorAtCurrentToken(`expected beginning of close tag (</...), got '${this.getTokenRangeStartingAt(this.getCurrentTokenIndex(), 2)}'`));
+			const message = `expected beginning of close tag (</...), got '${this.getTokenRangeStartingAt(this.getCurrentTokenIndex(), 2)}'`;
+			this.raiseError(this.createUnexpectedTokenSyntaxErrorAtCurrentToken(message));
 		}
 		// Skip over the tag opener:
 		//     </alpha ...
@@ -756,7 +778,8 @@ export class Parser {
 		if (this.getTokenRangeStartingAt(this.getCurrentTokenIndex(), 9) !== '<![CDATA[') {
 			this.raiseError(this.createUnexpectedTokenSyntaxErrorAtCurrentToken('expected beginning of CDATA section (<![CDATA[)'));
 		}
-		// We know this is actually a CDATA section node, so create the tree member and append to its content as long as it isn't closed by ']]>'.
+		// We know this is actually a CDATA section node, so create the tree member and append to its content as long
+		// as it isn't closed by ']]>'.
 		const cdataNode = new ast.CDataSectionNode();
 		this.getCurrentContainerNode().appendChild(cdataNode);
 		// Skip over the CDATA opener:
@@ -786,8 +809,8 @@ export class Parser {
 		if (this.getTokenRangeStartingAt(this.getCurrentTokenIndex(), 4) !== '<!--') {
 			this.raiseError(this.createUnexpectedTokenSyntaxErrorAtCurrentToken('expected beginning of comment (<!--)'));
 		}
-		// We know this is actually a comment node, so create the tree member and append to its content as long as the comment
-		// node is not closed by `-->`.
+		// We know this is actually a comment node, so create the tree member and append to its content as long as the
+		// comment node is not closed by `-->`.
 		const commentNode = new ast.CommentNode();
 		this.getCurrentContainerNode().appendChild(commentNode);
 		// Skip over the comment opener:
@@ -831,7 +854,9 @@ export class Parser {
 	
 	
 	/**
-	 * Parses a complete opening tag with namespace prefix, tag name and attributes into a given node. This method will decide whether the node it is parsing is a container node or a void node and upgrade the node passed into it in param `node` to the respective ast node type.
+	 * Parses a complete opening tag with namespace prefix, tag name and attributes into a given node. This method will
+	 * decide whether the node it is parsing is a container node or a void node and upgrade the node passed into it in
+	 * param `node` to the respective ast node type.
 	 * The cursor is expected to be pointing at the first token after the tag opener:
 	 * for "normal" nodes:
 	 *     <alpha ...
@@ -846,10 +871,15 @@ export class Parser {
 	 *     <?svg ...
 	 *       ^
 	 * @param node The node to parse namespace prefix, tag name and attributes into.
-	 * @param allowDescendingIntoNewContainerNode Whether the parser should be allowed to descend if this method discovers that the node it is parsing is a container node.
+	 * @param allowDescendingIntoNewContainerNode Whether the parser should be allowed to descend if this method
+	 * discovers that the node it is parsing is a container node.
 	 * @param allowSystemLiterals Whether system literals should be allowed in the parsed tag.
 	 */
-	protected parseCompleteOpeningTagInto(node: SelfClosingNode, allowDescendingIntoNewContainerNode: boolean, allowSystemLiterals: boolean): void {
+	protected parseCompleteOpeningTagInto(
+		node: SelfClosingNode,
+		allowDescendingIntoNewContainerNode: boolean,
+		allowSystemLiterals: boolean
+	): void {
 		// we could now be in any of the following constructs:
 		//     <alpha ...
 		//      ^
@@ -897,9 +927,12 @@ export class Parser {
 	
 	
 	/**
-	 * Parses the end of opening tags that are not self closing. This method will decide whether the node it is parsing is a container node or a void node and upgrade the node passed into it in param `node` to the respective ast node type.
+	 * Parses the end of opening tags that are not self closing. This method will decide whether the node it is parsing
+	 * is a container node or a void node and upgrade the node passed into it in param `node` to the respective ast node
+	 * type.
 	 * @param node The node to parse namespace prefix, tag name and attributes into.
-	 * @param allowDescendingIntoNewContainerNode Whether the parser should be allowed to descend if this method discovers that the node it is parsing is a container node.
+	 * @param allowDescendingIntoNewContainerNode Whether the parser should be allowed to descend if this method discovers
+	 *                                            that the node it is parsing is a container node.
 	 */
 	protected parseEndOfNonSelfClosingOpeningTag(node: SelfClosingNode, allowDescendingIntoNewContainerNode: boolean): void {
 		if (!(node instanceof SelfClosingNode)) {
@@ -943,14 +976,24 @@ export class Parser {
 		while (Parser.isTokenLegalInTagNameOrTagNameNamespacePrefix(this.getCurrentToken()) || this.getCurrentToken() === ':') {
 			if (this.getCurrentToken() === ':') {
 				if (colonSeen) {
-					this.raiseError(this.createSyntaxErrorAtCurrentToken(SyntaxErrorCode.IllegalNamespacePrefix, 'illegal multiple namespace prefix (multiple colons in tag name)'));
+					this.raiseError(
+						this.createSyntaxErrorAtCurrentToken(
+							SyntaxErrorCode.IllegalNamespacePrefix,
+							'illegal multiple namespace prefix (multiple colons in tag name)'
+						)
+					);
 				}
 				colonSeen = true;
 				tagNameInfo.namespacePrefix = nameStash;
 				nameStash = '';
 				this.advanceToNextToken();
 				if (!Parser.isAlphabeticToken(this.getCurrentToken())) {
-					this.raiseError(this.createSyntaxErrorAtCurrentToken(SyntaxErrorCode.MissingTagNameAfterNamespacePrefix, 'namespace prefix must be followed by a tag name'));
+					this.raiseError(
+						this.createSyntaxErrorAtCurrentToken(
+							SyntaxErrorCode.MissingTagNameAfterNamespacePrefix,
+							'namespace prefix must be followed by a tag name'
+						)
+					);
 					return;
 				}
 			}
@@ -974,8 +1017,8 @@ export class Parser {
 	
 	
 	protected parseAttributeListInto(node: Node, allowSystemLiterals: boolean): void {
-		// We are now at the first token after the opening tag name, which could be either whitespace, the end of the opening tag or
-		// the start of a system literal:
+		// We are now at the first token after the opening tag name, which could be either whitespace, the end of the
+		// opening tag or the start of a system literal:
 		//     <alpha fibo="nacci"...
 		//           ^
 		// or:
@@ -1013,8 +1056,8 @@ export class Parser {
 			} else {
 				let attrInfo = this.parseAttribute();
 				/// TODO:
-				/// Empty attribute names should never happen (see issue #7). Find out why this happens, fix it, then remove the
-				/// `continue` workaround below.
+				/// Empty attribute names should never happen (see issue #7). Find out why this happens, fix it, then
+				/// remove the `continue` workaround below.
 				if (attrInfo.name === '') {
 					continue;
 				}
@@ -1061,11 +1104,21 @@ export class Parser {
 		while (Parser.isTokenLegalInAttributeNameOrAttributeNameNameNamespacePrefix(this.getCurrentToken()) || this.getCurrentToken() === ':') {
 			if (this.getCurrentToken() === ':') {
 				if (colonSeen) {
-					this.raiseError(this.createSyntaxErrorAtCurrentToken(SyntaxErrorCode.IllegalNamespacePrefix, 'illegal multiple namespace prefix (multiple colons in tag name)'));
+					this.raiseError(
+						this.createSyntaxErrorAtCurrentToken(
+							SyntaxErrorCode.IllegalNamespacePrefix,
+							'illegal multiple namespace prefix (multiple colons in tag name)'
+						)
+					);
 				}
 				colonSeen = true;
 				if (!Parser.isAlphabeticToken(this.getNextToken())) {
-					this.raiseError(this.createSyntaxErrorAtCurrentToken(SyntaxErrorCode.MissingTagNameAfterNamespacePrefix, 'namespace prefix must be followed by a tag name'));
+					this.raiseError(
+						this.createSyntaxErrorAtCurrentToken(
+							SyntaxErrorCode.MissingTagNameAfterNamespacePrefix,
+							'namespace prefix must be followed by a tag name'
+						)
+					);
 					return;
 				}
 			}
